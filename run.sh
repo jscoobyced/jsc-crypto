7821a2fd2496964eab8fb188ad6a5bee980e72df
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-04-20 20:33:38 +0700

    Add to clean-up container.

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+docker stop jsc_crypto
+docker rm jsc_crypto
 docker pull jscdroiddev/jsc-crypto:latest
 docker run -d -p 8888:8888 --mount type=bind,source="$(pwd)"/data,target=/opt/jsc_crypto/data --name jsc_crypto --restart unless-stopped jscdroiddev/jsc-crypto:latest

12dfeab4906f0cfcce290630ea8e21ae44475181
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-04-20 20:21:53 +0700

    Add run script.

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker pull jscdroiddev/jsc-crypto:latest
+docker run -p 8888:8888 --mount type=bind,source="$(pwd)"/data,target=/opt/jsc_crypto/data --name jsc_crypto --restart unless-stopped jscdroiddev/jsc-crypto:latest

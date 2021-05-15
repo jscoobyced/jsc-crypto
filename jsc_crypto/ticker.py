af5653550f971439e76f76f3bca8d39f8ea54420
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-05-15 21:05:26 +0700

    Update to support binance.

--- jsc_crypto/ticker.py
+++ jsc_crypto/ticker.py
@@ -2,44 +2,50 @@
 
 import requests
 
-URL="https://api.tdax.com/api/v3/ticker/24hr?symbol="
-CURRENCY="THB"
-SELF="thb_thb"
-SYMBOLS=[["DOGE", "doge_thb"],["ADA","ada_thb"], ["BTC", "btc_thb"], ["THB", "thb_thb"]]
-PASS_LOW=0.6
-PASS_HIGH=0.6
-INDEX_DATE=0
-INDEX_QUANTITY=1
-INDEX_VALUE=2
-INDEX_COST=3
+SATANG_URL = "https://api.tdax.com/api/v3/ticker/24hr?symbol="
+BINANCE_URL = "https://api.binance.com/api/v3/ticker/24hr?symbol="
+URLS = [SATANG_URL, BINANCE_URL]
+CURRENCY = "THB"
+SELF = "thb_thb"
+SYMBOLS = [["DOGE", "doge_thb", 0, 1], ["ADA", "ada_thb", 0, 1],
+           ["BTC", "btc_thb", 0, 1], ["TRX", "trx_thb", 0, 1],
+            ["THB", "thb_thb", 0, 1], ["SHIB", "SHIBUSDT", 1, 31.25]]
+INDEX_DATE = 0
+INDEX_QUANTITY = 1
+INDEX_VALUE = 2
+INDEX_COST = 3
+
 
 def get_transactions(crypto, data):
     filename = f"{data}/{crypto}.csv"
-    file = open(filename,"r")
+    file = open(filename, "r")
     if not file:
         return []
     lines = file.readlines()
     transactions = []
     for line in lines:
         values = line.split()
-        transactions.append([values[0],float(values[1]),float(values[2]),float(values[3])])
+        transactions.append([values[0], float(values[1]),
+                             float(values[2]), float(values[3])])
     file.close()
     return transactions
 
-def get_latest(currency):
+
+def get_latest(currency, url):
     global SELF
     if SELF == currency:
         return float(1)
-    response = requests.get(f"{URL}{currency}")
+    response = requests.get(f"{url}{currency}")
     if response.status_code != 200:
         return -1
     json_data = response.json()
     return float(json_data['lastPrice'])
 
-def get_summary(currency, data):
+
+def get_summary(currency, url, factor, data):
     global INDEX_QUANTITY, INDEX_VALUE, INDEX_COST
     transactions = get_transactions(currency, data)
-    latest = get_latest(currency)
+    latest = get_latest(currency, url) * factor
     total_value = 0
     total_cost = 0
     total_quantity = 0
@@ -50,7 +56,8 @@ def get_summary(currency, data):
         total_value = total_value + current_value
     return [total_cost, total_value, latest, total_quantity]
 
-def format_result(summary, with_coin = False):
+
+def format_result(summary, with_coin=False):
     profit = "Profit"
     profit_loss = summary[1] - summary[0]
     if profit_loss < 0:
@@ -65,20 +72,22 @@ def format_result(summary, with_coin = False):
         coins = f"Coins: {total_quantity}, "
     return f"{coins}Cost: {total_cost}, Value: {total_value}, {profit}: {total_profit}"
 
+
 def get_data(data):
-  total_cost = 0
-  total_value = 0
-  total_quantity = 0
-  result = ""
+    total_cost = 0
+    total_value = 0
+    total_quantity = 0
+    result = ""
 
-  for symbol in SYMBOLS:
-      summary = get_summary(symbol[1], data)
-      result = result + f"Results for {symbol[0]}: {summary[2]}\n"
-      total_cost = total_cost + summary[0]
-      total_value = total_value + summary[1]
-      total_quantity = total_quantity + summary[3]
-      result = result + format_result(summary, True) + "\n"
+    for symbol in SYMBOLS:
+        summary = get_summary(symbol[1], URLS[symbol[2]], symbol[3], data)
+        result = result + f"Results for {symbol[0]}: {summary[2]}\n"
+        total_cost = total_cost + summary[0]
+        total_value = total_value + summary[1]
+        total_quantity = total_quantity + summary[3]
+        result = result + format_result(summary, True) + "\n"
 
-  result = result + "Final summary:\n"
-  result = result + format_result([total_cost, total_value, 0, total_quantity])
-  return result
+    result = result + "Final summary:\n"
+    result = result + \
+        format_result([total_cost, total_value, 0, total_quantity])
+    return result
